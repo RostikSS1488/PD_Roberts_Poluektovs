2bc3f773c735cc9d6b52b35ca173c640ad4107c6
RostikSS1488 <desertgambovich@gmail.com>
2026-03-04 14:34:54 +0200

    funk

--- funkcijas.py
+++ funkcijas.py
@@ -1,3 +1,7 @@
 Calc_cukurs
 Calc_cenavsya
 Calc_masavsya
+
+jabloki_kg = float(input("Ievadiet ābolu daudzumu kilogramos: : "))
+jabloki_evro = float(input("Ievadiet ābolu cenu euro par kilogramu:  "))
+cukurs_evro = float(input("Ievadiet cukura cenu euro par kilogramu:  "))
