e902f52bfdfd70a27557524fb378e8d861a0d086
RostikSS1488 <desertgambovich@gmail.com>
2026-03-04 14:36:41 +0200

    aprekini

--- funkcijas.py
+++ funkcijas.py
@@ -5,3 +5,6 @@ Calc_masavsya
 jabloki_kg = float(input("Ievadiet ābolu daudzumu kilogramos: : "))
 jabloki_evro = float(input("Ievadiet ābolu cenu euro par kilogramu:  "))
 cukurs_evro = float(input("Ievadiet cukura cenu euro par kilogramu:  "))
+
+jabloki_kg/2
+jabloki_kg*jabloki_evro + cukurs_kg*cukurs_evro
